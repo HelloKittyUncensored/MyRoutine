97592ea613acebcfc850ccd0a0dd50d88ec90601
HelloKittyUncensored <156320574+HelloKittyUncensored@users.noreply.github.com>
2026-03-29 23:00:31 +0200

    Delete training directory

--- training/training.js
+++
@@ -1,9 +0,0 @@
-// hover navigatie
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        gsap.to(link, { color: "#e63946", duration: 0.3 });
-    });
-    link.addEventListener('mouseleave', () => {
-        gsap.to(link, { color: "#fff", duration: 0.3 });
-    });
-});
